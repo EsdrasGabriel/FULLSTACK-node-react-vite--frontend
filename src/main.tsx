482cce617c0170da2bfb2fd1f45bb068cea4bf26
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-11-01 11:44:10 -0300

    feat: added LightTheme

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./shared/themes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={LightTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import { Button } from "@mui/material";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Button variant="contained" color="primary">Teste</Button>,
   },
   {
     path: "*",
