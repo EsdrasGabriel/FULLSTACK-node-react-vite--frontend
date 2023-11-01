3c87d53407c73626dfd9b88b8a8bc8c5890b6711
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-11-01 10:58:12 -0300

    feat: added routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export {};
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/"/>
+  },
+]);
