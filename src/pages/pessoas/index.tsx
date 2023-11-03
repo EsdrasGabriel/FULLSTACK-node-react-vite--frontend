ef25027bfc9aee9fd59798fa5fe24d05a0145521
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-11-02 23:55:55 -0300

    feat: added search and pagination

--- src/pages/pessoas/index.tsx
+++ src/pages/pessoas/index.tsx
@@ -1,41 +1,45 @@
 import { useEffect, useMemo, useState } from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
 
 import { IPeopleListing, PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { BaseLayout } from '../../shared/layouts/BaseLayout';
 import { ListingTools } from '../../shared/components';
 import { useDebounce } from '../../shared/hooks';
+import { Environment } from '../../shared/environment';
 
 export const ListOfPeople = () => {
   const [ searchParams, setSearchParams ] = useSearchParams();
+
   const { debounce } = useDebounce(3000);
 
   const [rows, setRows] = useState<IPeopleListing[]>([]);
   const [totalCount, setTotalCount] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const search = useMemo(() => {
+  const busca = useMemo(() => {
     return searchParams.get('busca') || '';
+  }, [searchParams]);
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1');
   }, [searchParams]);
 
   useEffect(() => {
     setIsLoading(true);
 
     debounce(() => {
-      PessoasService.getAll(1, search)
+      PessoasService.getAll(pagina, busca)
         .then((result) => {
           setIsLoading(false);
           if (result instanceof Error) {
             alert(result.message);
           } else {
-            console.log(result);
             setRows(result.data);
             setTotalCount(result.totalCount);
           }
         });
     });
-  }, [search]);
+  }, [busca, pagina]);
 
   return (
     <BaseLayout 
@@ -44,8 +48,8 @@ export const ListOfPeople = () => {
         <ListingTools 
           textButton='Nova'
           showInputSearch
-          searchText={search}
-          byChangingSearchText={text => setSearchParams({ busca: text }, { replace: true })}
+          searchText={busca}
+          byChangingSearchText={text => setSearchParams({ busca: text, pagina: '1' }, { replace: true })}
         />
       }
     >
@@ -67,6 +71,32 @@ export const ListOfPeople = () => {
             </TableRow>
             ))}
           </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.EMPTY_LISTING}</caption>
+          )}
+
+          <TableFooter>
+                {isLoading &&
+                  <TableRow>
+                    <TableCell colSpan={3}>
+                        <LinearProgress variant='indeterminate'/>
+                    </TableCell>
+                  </TableRow>
+                }
+                {(totalCount > 0 && totalCount > Environment.ROW_LIMIT) &&
+                  <TableRow>
+                    <TableCell colSpan={3}>
+                      <Pagination 
+                        color="primary" 
+                        page={pagina}
+                        count={Math.ceil(totalCount / Environment.ROW_LIMIT)}
+                        onChange={(_, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                        />
+                    </TableCell>
+                  </TableRow>
+                }
+          </TableFooter>
         </Table>
       </TableContainer>
     </BaseLayout>
