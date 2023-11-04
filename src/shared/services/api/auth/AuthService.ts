ca3209c27fbdc16e14c3765358672a6e9fb8b5a1
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2023-11-04 02:05:04 -0300

    feat: added auth

---
+++ src/shared/services/api/auth/AuthService.ts
@@ -0,0 +1,20 @@
+import { Api } from '../axios-config';
+
+interface IAuth {
+  accessToken: string;
+}
+
+const auth = async (email: string, password: string): Promise<IAuth | Error> => {
+  try {
+    const { data } = await Api.get('/auth', { data: { email, password } });
+
+    if (data) return data;
+
+    return new Error('Erro no login.');
+  } catch (err) {
+    console.error(err);
+    return new Error((err as { message: string }).message || 'Erro no login.');
+  }
+};
+
+export const AuthService = { auth };
